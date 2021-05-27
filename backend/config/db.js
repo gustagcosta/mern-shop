3b578916c9db2e30b6ac964841f9846521cf2244
gus <gustavoprof3@gmail.com>
2021-05-27 00:19:25 -0300

    mongo db setup, init models

---
+++ backend/config/db.js
@@ -0,0 +1,18 @@
+import mongoose from 'mongoose';
+
+const connectDb = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URL, {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+      useCreateIndex: true,
+    });
+
+    console.log(`Mongo DB connected: ${conn.connection.host}`.cyan.underline);
+  } catch (error) {
+    console.error(`Error ${error}`.red.underline.bold);
+    process.exit(1);
+  }
+};
+
+export default connectDb;
--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,12 @@
-const express = require('express');
-const dotenv = require('dotenv');
-const products = require('./data/products');
+import express from 'express';
+import { config } from 'dotenv';
+import colors from 'colors';
+import products from './data/products.js';
+import connectDb from './config/db.js';
 
-dotenv.config();
+config();
+
+connectDb();
 
 const app = express();
 
@@ -15,11 +19,13 @@ app.get('/api/products', (req, res) => {
 });
 
 app.get('/api/products/:id', (req, res) => {
-  res.json(products.find((p) => p._id === req.params.id));
+  res.json(find((p) => p._id === req.params.id));
 });
 
 const PORT = process.env.PORT || 3333;
 
 app.listen(PORT, () =>
-  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.blue.bold
+  )
 );
